ea617c1295de51051db8069b625c9d13ab3bc7c7
25ejsb <eitan.brochstein@icloud.com>
2024-10-13 12:53:04 -0400

    example

--- Selenium/main.py
+++ Selenium/main.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchAttributeException
+from time import sleep as wait
 # if on mac, do brew install chromedriver
 
 # use virtualenv to create enviornment
 
 driver = webdriver.Chrome()
-driver.get("https://google.com")
+driver.get("https://x.com/login")
+
+wait(3)
+
+signin = driver.find_element(By.CSS_SELECTOR, "input")
+signin.send_keys("hello")
 
 while True:
     pass
