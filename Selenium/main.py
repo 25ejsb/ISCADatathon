10e2b11b1e733849dcd22b774d9152ae16a12775
25ejsb <eitan.brochstein@icloud.com>
2024-10-13 12:36:52 -0400

    virtualenv

--- Selenium/main.py
+++ Selenium/main.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchAttributeException
 # if on mac, do brew install chromedriver
 
+# use virtualenv to create enviornment
+
 driver = webdriver.Chrome()
 driver.get("https://google.com")
 
