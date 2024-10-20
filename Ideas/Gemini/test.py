4d7239120e4771f16f3fc4c7a61e6f0d9e2b0087
Logdablobber <139921433+Logdablobber@users.noreply.github.com>
2024-10-20 16:11:57 -0400

    Fixed Gemini
    Removed safety blocks to fix issue where Gemini wouldn't read the tweet

--- Ideas/Gemini/test.py
+++ Ideas/Gemini/test.py
@@ -1,13 +1,20 @@
 API_KEY = "AIzaSyD5-Vr5C-kamIeYShZW1JcFhIdYTY2RyJM"
 
 import google.generativeai as genai
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
 import os
 
 genai.configure(api_key=API_KEY)
 
 model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("On a scale of 1 through 10, give a rating on how anti-semetic this tweet is: You realize that “children of Israel” means “children of Jacob,” because he was given the name Israel after wrestling an angel? It does not mean the land of Israel and certainly not the modern state of Israel.")
-with open("result.txt", "w") as file:
-    file.write(response.text)
+response = model.generate_content(
+    "On a scale of 1 through 10, give a rating on how anti-semetic this tweet is: You realize that “children of Israel” means “children of Jacob,” because he was given the name Israel after wrestling an angel? It does not mean the land of Israel and certainly not the modern state of Israel.",
+    safety_settings={
+        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE
+    })
+
+print(response.text)
 
 # this code doesnt work with python 3.13 for some reason
