583d73a5136415da6b40f26559804c0e8fbfc7b2
Logdablobber <139921433+Logdablobber@users.noreply.github.com>
2024-11-06 16:30:19 -0500

    Added better Gemini output

--- Ideas/Gemini/test.py
+++ Ideas/Gemini/test.py
@@ -3,20 +3,28 @@ API_KEY = "AIzaSyD5-Vr5C-kamIeYShZW1JcFhIdYTY2RyJM"
 import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
 import os
+import pandas
 
 genai.configure(api_key=API_KEY)
 
 model = genai.GenerativeModel(
     "gemini-1.5-flash",
-    system_instruction="Give a rating, on a scale of 1 (least) through 10 (most), of how overall anti-semetic this tweet is. Explain your thought process.")
+    system_instruction="Give a rating, on a scale of 1 (least) through 100 (most), of how overall anti-semetic each of these tweets are.")
+
+pd = pandas.read_csv("./SpaCy/GoldStandard2024_Participants.csv")
 
 response = model.generate_content(
-    "You realize that “children of Israel” means “children of Jacob,” because he was given the name Israel after wrestling an angel? It does not mean the land of Israel and certainly not the modern state of Israel.",
+    f"{[str(row.Text) + ', ' for item, row in pd.head(100).iterrows()]}",
     safety_settings={
         HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
         HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
         HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE
-    })
+    },
+    generation_config={
+        "response_mime_type": "application/json",
+        "response_schema": list[int]
+    }
+    )
 
 print(response.text)
 
